a19a483486076af861beba29b36014cc3b68c521
adpVnext <jacques.pupponi@vnext.fr>
2017-05-12 15:04:23 +0200

    Crash log en base sqlite via JsonStore

---
+++ Source/KSCrash/Installations/KSCrashInstallationPomLog.m
@@ -0,0 +1,80 @@
+//
+//  KSCrashInstallationPomLog.m
+//  KSCrash-iOS
+//
+//  Copyright (c) 2012 Karl Stenerud. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall remain in place
+// in this source code.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import "KSCrashInstallationPomLog.h"
+
+#import <KSCrash/KSCrashInstallation+Private.h>
+#import "KSCrashReportSinkPomLog.h"
+#import <KSCrash/KSCrashReportFilterAppleFmt.h>
+#import <KSCrash/KSCrashReportFilterBasic.h>
+#import <KSCrash/KSCrashReportFilterJSON.h>
+#import <KSCrash/KSCrashReportFilterStringify.h>
+
+@implementation KSCrashInstallationPomLog
+
+@synthesize printAppleFormat = _printAppleFormat;
+
++ (instancetype) sharedInstance
+{
+    static KSCrashInstallationPomLog *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[KSCrashInstallationPomLog alloc] init];
+    });
+    return sharedInstance;
+}
+
+- (id) init
+{
+    if((self = [super initWithRequiredProperties:nil]))
+    {
+        self.printAppleFormat = YES;
+    }
+    return self;
+}
+
+- (id<KSCrashReportFilter>) sink
+{
+    id<KSCrashReportFilter> formatFilter;
+    if(self.printAppleFormat)
+    {
+        formatFilter = [KSCrashReportFilterAppleFmt filterWithReportStyle:KSAppleReportStyleSymbolicated];
+    }
+    else
+    {
+        formatFilter = [KSCrashReportFilterPipeline filterWithFilters:
+                        [KSCrashReportFilterJSONEncode filterWithOptions:KSJSONEncodeOptionPretty | KSJSONEncodeOptionSorted],
+                        [KSCrashReportFilterStringify new],
+                        nil];
+    }
+
+    return [KSCrashReportFilterPipeline filterWithFilters:
+            formatFilter,
+            [KSCrashReportSinkPomLog new],
+            nil];
+}
+
+@end
